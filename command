6e1bcafe7a94436cd67655b248093e1116d4a294
ironcadiz <afcadiz@uc.cl>
2019-04-09 10:20:41 -0400

    pascalVOC training runnning

---
+++ command
@@ -0,0 +1,1 @@
+CUDA_VISIBLE_DEVICES=0 python trainval_net.py --dataset pascal_voc --net res101  --bs 1 --nw 2 --lr 0.001 --lr_decay_step 5 --cuda
--- lib/datasets/factory.py
+++ lib/datasets/factory.py
@@ -12,7 +12,7 @@ from __future__ import print_function
 
 __sets = {}
 from datasets.pascal_voc import pascal_voc
-from datasets.coco import coco
+# from datasets.coco import coco
 from datasets.imagenet import imagenet
 from datasets.vg import vg
 
@@ -25,22 +25,22 @@ for year in ['2007', '2012']:
     __sets[name] = (lambda split=split, year=year: pascal_voc(split, year))
 
 # Set up coco_2014_<split>
-for year in ['2014']:
-  for split in ['train', 'val', 'minival', 'valminusminival', 'trainval']:
-    name = 'coco_{}_{}'.format(year, split)
-    __sets[name] = (lambda split=split, year=year: coco(split, year))
+# for year in ['2014']:
+#   for split in ['train', 'val', 'minival', 'valminusminival', 'trainval']:
+#     name = 'coco_{}_{}'.format(year, split)
+#     __sets[name] = (lambda split=split, year=year: coco(split, year))
 
-# Set up coco_2014_cap_<split>
-for year in ['2014']:
-  for split in ['train', 'val', 'capval', 'valminuscapval', 'trainval']:
-    name = 'coco_{}_{}'.format(year, split)
-    __sets[name] = (lambda split=split, year=year: coco(split, year))
+# # Set up coco_2014_cap_<split>
+# for year in ['2014']:
+#   for split in ['train', 'val', 'capval', 'valminuscapval', 'trainval']:
+#     name = 'coco_{}_{}'.format(year, split)
+#     __sets[name] = (lambda split=split, year=year: coco(split, year))
 
-# Set up coco_2015_<split>
-for year in ['2015']:
-  for split in ['test', 'test-dev']:
-    name = 'coco_{}_{}'.format(year, split)
-    __sets[name] = (lambda split=split, year=year: coco(split, year))
+# # Set up coco_2015_<split>
+# for year in ['2015']:
+#   for split in ['test', 'test-dev']:
+#     name = 'coco_{}_{}'.format(year, split)
+#     __sets[name] = (lambda split=split, year=year: coco(split, year))
 
 # Set up vg_<split>
 # for version in ['1600-400-20']:
